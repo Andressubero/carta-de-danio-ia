fd0ab8ef12436079c2ac538f1233d7141b02b2cc
Andres Subero <andressuberoh@gmail.com>
2025-05-08 19:52:51 -0300

    feat (sql-integration)

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
 import os
 
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://miusuario:miclave123@localhost:3306/carta_danio_ia'
+    SQL_USER = os.getenv("SQL_USER", "miusuario")
+    SQL_PASSWORD = os.getenv("SQL_PASSWORD", "miclave123")
+    SQL_DATABASE_NAME = os.getenv("SQL_DATABASE_NAME", "carta_danio_ia")
+    SQL_PORT = os.getenv("SQL_PORT", "3306")
+
+    SQLALCHEMY_DATABASE_URI = f'mysql+mysqldb://{SQL_USER}:{SQL_PASSWORD}@localhost:{SQL_PORT}/{SQL_DATABASE_NAME}'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'uploads'
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "TU_API_KEY_AQUÍ")
