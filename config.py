54e5b3b3f381b0bcb41256ccf6351110f9392af7
juancruzc <juancruzcobo@hotmail.com>
2025-05-15 21:48:22 -0300

    fix: variables de entorno y librerias

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 import os
-
-import os
+from dotenv import load_dotenv
 
 class Config:
+    load_dotenv()
     SQL_USER = os.getenv("SQL_USER", "miusuario")
     SQL_PASSWORD = os.getenv("SQL_PASSWORD", "miclave123")
     SQL_DATABASE_NAME = os.getenv("SQL_DATABASE_NAME", "carta_danio_ia")
     SQL_PORT = os.getenv("SQL_PORT", "3306")
+
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
     SQLALCHEMY_DATABASE_URI = f'mysql+mysqldb://{SQL_USER}:{SQL_PASSWORD}@localhost:{SQL_PORT}/{SQL_DATABASE_NAME}'
 
