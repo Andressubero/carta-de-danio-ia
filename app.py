dbb5f78d5c189ec3bbe95844a8c014a4ed40d7d4
juancruzc <juancruzcobo@hotmail.com>
2025-06-01 15:16:13 -0300

    feat: modulo de ia para su llamado desde el back

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-import google.generativeai as genai
 from flask import Flask, render_template, request
 from sqlalchemy import text
 from config import Config
@@ -10,6 +9,7 @@ from models.models import Base
 from routes.UserRoutes import user_bp  # Importá el Base desde tu models
 from routes.vehicles_routes import vehicle_bp 
 from routes.vehicle_state_routes import vehicle_state_bp 
+from utils.ai import call_llm
 
 load_dotenv()
 app = Flask(__name__)
@@ -24,9 +24,6 @@ db.init_app(app)
 # Configurar carpeta de uploads
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
-# Configurá tu API KEY de Gemini
-genai.configure(api_key=app.config['GEMINI_API_KEY'])
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     result = ""
@@ -39,39 +36,38 @@ def index():
             image_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
             image.save(image_path)
 
-            with open(image_path, "rb") as img_file:
-                img_bytes = img_file.read()
-
-            parts = [{"text": prompt},
-                     {"inline_data": {
-                         "mime_type": "image/jpeg",
-                         "data": img_bytes
-                     }}]
-
+            ref_image_path = ''
             if ref_image and ref_image.filename != "":
                 ref_image_path = os.path.join(app.config['UPLOAD_FOLDER'], ref_image.filename)
                 ref_image.save(ref_image_path)
-                with open(ref_image_path, "rb") as ref_img_file:
-                    ref_img_bytes = ref_img_file.read()
 
-                # Añadir mensaje contextual
-                parts.insert(0, {"text": "Compará el estado actual del vehículo con la imagen de referencia anterior, identificando solo los daños nuevos si los hubiera."})
-                parts.append({"inline_data": {
-                    "mime_type": "image/jpeg",
-                    "data": ref_img_bytes
-                }})
-
-            model = genai.GenerativeModel("gemini-1.5-flash")
+            llm_data = [
+	            {	#imagen lateral izquierda
+	        	    'image': image_path,
+	        	    'parts': [
+                        { 
+                            'part': 'frente delantero antes', 
+                            'damages': []
+                        }
+                    ]
+                },
+	            {	#imagen lateral derecha
+	        	    'image': ref_image_path,
+	        	    'parts': [
+                        { 
+                            'part': 'frente delantero despues', 
+                            'damages': ['abolladura', 'rayon']
+                        }
+                    ]
+                }
+            ]
 
             try:
-                response = model.generate_content([
-                    {"role": "user", "parts": parts}
-                ])
-                result = response.text
+                result = call_llm(llm_data, 'COMP')
             except Exception as e:
-                result = f"Error: {e}"
+                result = f'Error: {e}'
 
-    return render_template("index.html", result=result)
+    return render_template('index.html', result=result)
 
 # app.py
 
@@ -79,9 +75,9 @@ if __name__ == '__main__':
     with app.app_context():
         try:
             db.session.execute(text('SELECT 1'))
-            print("✅ Conexión exitosa a la base de datos.")
+            print('✅ Conexión exitosa a la base de datos.')
         except Exception as e:
-            print(f"❌ Error de conexión a la base de datos: {e}")
+            print(f'❌ Error de conexión a la base de datos: {e}')
         Base.metadata.create_all(bind=db.engine)
 
         from models.parts_seed import seed_parts
