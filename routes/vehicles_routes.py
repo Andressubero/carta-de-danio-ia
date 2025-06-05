5cd5fd3e4ca9257108ba8921064a6af058569b93
Francisco Vecchia <franvecchia00@gmail.com>
2025-06-05 16:12:44 -0300

    fix conflicto vehiculo

--- routes/vehicles_routes.py
+++ routes/vehicles_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from controllers.vehicle_controller import create_vehicle, get_vehicles_by_user_controller
+from controllers.vehicle_controller import create_vehicle, get_vehicles_by_user_controller, get_vehicle_by_id
 from auth.utils import token_required
 
 vehicle_bp = Blueprint('vehicles', __name__, url_prefix='/vehicle')
@@ -11,11 +11,5 @@ def create_vehicle_route(): return create_vehicle()
 @vehicle_bp.route('/myVehicles', methods=['GET'])
 @token_required
 def get_vehicles_by_user(): return get_vehicles_by_user_controller()
-from controllers.vehicle_controller import create_vehicle
-from controllers.vehicle_controller import get_vehicle_by_id  
 
-from auth.utils import token_required
-
-vehicle_bp = Blueprint('vehicles', __name__, url_prefix='/vehicle')
-vehicle_bp.route('/create', methods=['POST'])(create_vehicle)
-vehicle_bp.route('/:id/', methods=['GET'])(get_vehicle_by_id)  # Assuming this is a placeholder for a get vehicle by ID route
+vehicle_bp.route('/:id/', methods=['GET'])(get_vehicle_by_id)
