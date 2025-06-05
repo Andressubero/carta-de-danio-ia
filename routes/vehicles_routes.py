ea7995a29ef934510c7cbb05f276b4a3794802af
Federico Nintzel <FedeNintzel@hotmail.com>
2025-06-05 10:29:49 -0600

    get vehicle by id endpoint

--- routes/vehicles_routes.py
+++ routes/vehicles_routes.py
@@ -1,6 +1,9 @@
 from flask import Blueprint
 from controllers.vehicle_controller import create_vehicle
+from controllers.vehicle_controller import get_vehicle_by_id  
+
 from auth.utils import token_required
 
 vehicle_bp = Blueprint('vehicles', __name__, url_prefix='/vehicle')
 vehicle_bp.route('/create', methods=['POST'])(create_vehicle)
+vehicle_bp.route('/:id/', methods=['GET'])(get_vehicle_by_id)  # Assuming this is a placeholder for a get vehicle by ID route
