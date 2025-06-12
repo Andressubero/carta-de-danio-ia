08f2173b2a936fca9edcf28fb40643ec2c668b89
Andres Subero <andressuberoh@gmail.com>
2025-06-12 20:55:07 -0300

    feat (use-params-vehicle-state)

--- routes/vehicle_state_routes.py
+++ routes/vehicle_state_routes.py
@@ -4,12 +4,12 @@ from auth.utils import token_required
 
 vehicle_state_bp = Blueprint('states', __name__, url_prefix='/vehicle-state')
 
-vehicle_state_bp.route('/create', methods=['POST'])
+@vehicle_state_bp.route('/create', methods=['POST'])
 @token_required
 def create_vehicle_state_route(): return create_vehicle_state()
 
 
-vehicle_state_bp.route('/get-all', methods=['GET'])
+@vehicle_state_bp.route('/get-all', methods=['GET'])
 @token_required
 def get_all_vehicle_state_route(): return get_all_vehicle_state()
 
