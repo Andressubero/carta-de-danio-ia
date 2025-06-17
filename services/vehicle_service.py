0cbacb168542b9c2f1e8b0e54718f98ff391ef02
Francisco Vecchia <franvecchia00@gmail.com>
2025-06-17 20:56:37 -0300

    edit vehiculo

--- services/vehicle_service.py
+++ services/vehicle_service.py
@@ -45,3 +45,35 @@ def get_vehicle_with_parts(vehicle_id):
 
 def get_vehicles(user_id):
     return VehicleRepository.get_all_by_user(user_id)
+
+def edit(vehicle_id, user_id, vehicle_type_id, model, brand, year, plate):
+    vehicle = VehicleRepository.get_by_id(vehicle_id)
+    
+    if not vehicle:
+        return {
+            'message': 'Vehículo no encontrado',
+            'success': False
+        }, 200
+
+    if str(vehicle.user_id) != user_id:
+        return {
+            'message': 'No tienes permiso para editar este vehículo',
+            'success': False
+        }, 200
+
+    vehicle.vehicle_type_id = vehicle_type_id
+    vehicle.model = model
+    vehicle.brand = brand
+    vehicle.year = year
+    vehicle.plate = plate
+
+    try:
+        VehicleRepository.save(vehicle)
+        return {
+            'message': 'Vehículo actualizado con éxito',
+            'success': True,
+        }, 200
+    except Exception as e:
+        return {
+            'message': f'Error al actualizar vehículo: {str(e)}'
+        }, 500
